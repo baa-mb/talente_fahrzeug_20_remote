0aca23cd48e894ffc033852f3764fded82a8aac9
baa <34665120+baa-mb@users.noreply.github.com>
2026-05-13 18:02:10 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,18 @@
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
-    B_state = (B_state + 1) % 2
-    radio.sendValue("TB", B_state)
-    a_bilder[B_state].showImage(0)
-})
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_CLICK, function () {
-    B_state = (B_state + 1) % 2
-    radio.sendValue("TB", B_state)
-})
-let a_bilder: Image[] = []
-let B_state = 0
-radio.setGroup(3)
-basic.showString("U2")
-basic.showArrow(ArrowNames.West)
-B_state = 0
-let testFlag = 0
-a_bilder = [images.arrowImage(ArrowNames.North), images.iconImage(IconNames.Diamond)]
+let kurve = 0
+let gerade = 0
+let alt_kurve = 0
+let alt_gerade = 0
+radio.setGroup(26)
+basic.showString("U26")
+basic.showArrow(ArrowNames.South)
+let empfind = 4
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendValue("pitch", input.rotation(Rotation.Pitch))
-        radio.sendValue("roll", Math.idiv(input.rotation(Rotation.Roll), 2))
-    } else {
-        radio.sendValue("pitch", 0)
-        radio.sendValue("roll", 0)
-    }
+    gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
+    gerade = Math.round(gerade / empfind) * empfind
+    radio.sendValue("gerade", gerade)
+    basic.pause(100)
+    kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
+    kurve = Math.round(kurve / empfind) * empfind
+    radio.sendValue("kurve", kurve)
+    basic.pause(100)
 })
