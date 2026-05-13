fbbfba40eafd3c80ecd802626f0c88d445ecfcaf
baa <34665120+baa-mb@users.noreply.github.com>
2026-05-13 18:16:37 +0200

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,24 @@ let alt_kurve = 0
 let alt_gerade = 0
 radio.setGroup(26)
 basic.showString("U26")
-basic.showArrow(ArrowNames.South)
+// basic.showArrow(ArrowNames.South)
+basic.showString("<-")
 let empfind = 4
 basic.forever(function () {
     gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
     gerade = Math.round(gerade / empfind) * empfind
     radio.sendValue("gerade", gerade)
-    basic.pause(100)
+    // if (gerade < 0)
+    //     basic.showArrow(ArrowNames.North)
+    // else if (gerade > 0)
+    //     basic.showArrow(ArrowNames.South)
+
+    // if (kurve < 0)
+    //     basic.showArrow(ArrowNames.West)
+    // else if (kurve > 0)
+    //     basic.showArrow(ArrowNames.East)
+
+    // basic.pause(100)
     kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
     kurve = Math.round(kurve / empfind) * empfind
     radio.sendValue("kurve", kurve)
